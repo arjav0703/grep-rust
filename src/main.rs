048cb49903fd60f7c0a108fe6bac11a3ab74ad86
Arjav Jain <arjavjain0703@gmail.com>
2025-09-23 12:11:01 +0530

    add regex function

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         input_line.contains(pattern)
-    } else if pattern == r"\d" {
-        input_line.chars().any(|c| c.is_ascii_digit())
+    } else if pattern.starts_with(r"\") && pattern.len() == 2 {
+        regex_handler(input_line, pattern)
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
@@ -28,5 +28,20 @@ fn main() {
         process::exit(0)
     } else {
         process::exit(1)
+    }
+}
+
+fn regex_handler(input_line: &str, pattern: &str) -> bool {
+    let special_char = pattern.chars().nth(1).unwrap();
+    match special_char {
+        'd' => input_line.chars().any(|c| c.is_ascii_digit()),
+        'D' => input_line.chars().any(|c| !c.is_ascii_digit()),
+        's' => input_line.chars().any(|c| c.is_whitespace()),
+        'S' => input_line.chars().any(|c| !c.is_whitespace()),
+        'w' => input_line.chars().any(|c| c.is_alphanumeric() || c == '_'),
+        'W' => input_line
+            .chars()
+            .any(|c| !(c.is_alphanumeric() || c == '_')),
+        _ => panic!("Unhandled special character: {}", special_char),
     }
 }
